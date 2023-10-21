477d7bd6a260014133c1364108a635299e7b0095
gohyenther <goh.yenther@gmail.com>
2023-10-21 19:12:31 +0800

    add reverse str method in Mutator

--- student_fuzzer.py
+++ student_fuzzer.py
@@ -5,6 +5,7 @@ from fuzzingbook import MutationFuzzer as mf
 import traceback
 import numpy as np
 import time
+import random
 
 from bug import entrypoint
 from bug import get_initial_corpus
@@ -17,13 +18,12 @@ class MyCoverage(cv.Coverage):
     def coverage(self):
         # <your implementation here>
         
-        # purpose: to track the execution of control flow edges
-        print("start of execution:")
+        # purpose: to track the sequence of executions
+        traces = []
         for trace in self.trace():
-            print(trace, "->")
-        print("end")
+            traces.append(trace)
         
-        return set(self.trace())
+        return traces
 
 
 ## You can re-implement the runner class to change how
@@ -63,6 +63,58 @@ class MyRunner(mf.FunctionRunner):
 ## The Mutator and Schedule classes can also be extended or
 ## replaced by you to create your own fuzzer!
 
+class MyMutator:
+    """Mutate strings"""
+
+    def __init__(self) -> None:
+        """Constructor"""
+        self.mutators = [
+            self.delete_random_character,
+            self.insert_random_character,
+            self.flip_random_character,
+            self.reverse_str
+        ]
+
+class MyMutator(MyMutator):
+    def insert_random_character(self, s: str) -> str:
+        """Returns s with a random character inserted"""
+        pos = random.randint(0, len(s))
+        random_character = chr(random.randrange(32, 127))
+        return s[:pos] + random_character + s[pos:]
+
+class MyMutator(MyMutator):
+    def delete_random_character(self, s: str) -> str:
+        """Returns s with a random character deleted"""
+        if s == "":
+            return self.insert_random_character(s)
+
+        pos = random.randint(0, len(s) - 1)
+        return s[:pos] + s[pos + 1:]
+
+class MyMutator(MyMutator):
+    def flip_random_character(self, s: str) -> str:
+        """Returns s with a random bit flipped in a random position"""
+        if s == "":
+            return self.insert_random_character(s)
+
+        pos = random.randint(0, len(s) - 1)
+        c = s[pos]
+        bit = 1 << random.randint(0, 6)
+        new_c = chr(ord(c) ^ bit)
+        return s[:pos] + new_c + s[pos + 1:]
+
+class MyMutator(MyMutator):
+    def reverse_str(self, s: str) -> str:
+        return s[::-1]
+
+class MyMutator(MyMutator):
+    def mutate(self, inp: object()) -> object():  # can be str or Seed (see below)
+        """Return s with a random mutation applied. Can be overloaded in subclasses."""
+        # perform more than one random mutations
+        for _ in range(random.randint(1, 100)):
+            mutator = random.choice(self.mutators)
+            inp = mutator(inp)
+        return inp
 
     
 # When executed, this program should run your fuzzer for a very 
@@ -79,5 +131,5 @@ if __name__ == "__main__":
     # line_runner = mf.FunctionCoverageRunner(entrypoint)
     line_runner = MyRunner(entrypoint)
 
-    fast_fuzzer = gbf.CountingGreyboxFuzzer(seed_inputs, gbf.Mutator(), fast_schedule)
+    fast_fuzzer = gbf.CountingGreyboxFuzzer(seed_inputs, MyMutator(), fast_schedule)
     fast_fuzzer.runs(line_runner, trials=999999999)
