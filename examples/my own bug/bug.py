c128be08e5d7490422f11e94d0286b4e9a375fcb
gohyenther <goh.yenther@gmail.com>
2023-10-24 19:12:09 +0800

    update my own bug program

--- examples/my own bug/bug.py
+++ examples/my own bug/bug.py
@@ -1,12 +1,7 @@
 def get_initial_corpus():
-    return ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
-
+    return ["aaaaaaa"]
 
 def entrypoint(s):
-    x = 0
-    if len(s) > 2 and s[2] == 'B':
-        if len(s) > 3 and s[3] == 'A':
-            if len(s) > 4 and s[4] == 'D':
-                if len(s) > 5 and s[5] == '!':
-                    print("Found the bug!")
-                    exit(219)
+    if s == "AAAAAAA":
+        print("Found the bug!")
+        exit(219)
